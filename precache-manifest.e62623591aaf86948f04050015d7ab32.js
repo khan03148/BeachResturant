5a097779302bf7910edb12897b60cd7d03e7b8a2
Jahangir khan <khan03148@gmail.com>
2020-03-24 13:07:05 +0100

    Updates

---
+++ precache-manifest.e62623591aaf86948f04050015d7ab32.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f515ebde3054f3546abd8bb0c054834",
+    "url": "/khan03148/BeachResturant.git/index.html"
+  },
+  {
+    "revision": "73102aafdeca9a541a4e",
+    "url": "/khan03148/BeachResturant.git/static/css/main.3ceb68b1.chunk.css"
+  },
+  {
+    "revision": "122a7706f9e966fa450f",
+    "url": "/khan03148/BeachResturant.git/static/js/2.6d36934c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/khan03148/BeachResturant.git/static/js/2.6d36934c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73102aafdeca9a541a4e",
+    "url": "/khan03148/BeachResturant.git/static/js/main.d5b86f54.chunk.js"
+  },
+  {
+    "revision": "1d17e65068bd69adf36a",
+    "url": "/khan03148/BeachResturant.git/static/js/runtime-main.523c4899.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/khan03148/BeachResturant.git/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/khan03148/BeachResturant.git/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/khan03148/BeachResturant.git/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/khan03148/BeachResturant.git/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/khan03148/BeachResturant.git/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/khan03148/BeachResturant.git/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/khan03148/BeachResturant.git/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/khan03148/BeachResturant.git/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/khan03148/BeachResturant.git/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/khan03148/BeachResturant.git/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/khan03148/BeachResturant.git/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/khan03148/BeachResturant.git/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/khan03148/BeachResturant.git/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/khan03148/BeachResturant.git/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/khan03148/BeachResturant.git/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/khan03148/BeachResturant.git/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/khan03148/BeachResturant.git/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/khan03148/BeachResturant.git/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/khan03148/BeachResturant.git/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
